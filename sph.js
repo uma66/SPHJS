ad97a48c6bdcf4863e4025830a8f7320c51a4e52
uma6 <uma6@github.com>
2016-04-10 00:01:17 +0900

    Modify comment pos

--- sph.js
+++ sph.js
@@ -11,7 +11,7 @@ var DIMENSION       = 2;	// 2次元 or 3次元
 
 var PARTICLES_NUM   = 300;	// 粒子数
 var particles       = new Array(PARTICLES_NUM);
-var PARTICLE_RADIUS = 4;	// 粒子の半径
+var PARTICLE_RADIUS   = 4;	// 粒子の半径
 var SCOPE_H         = 30;	// 近傍探索範囲
 
 var polyCoef        = 0.0;	// poly6カーネル係数
@@ -22,14 +22,14 @@ window.onload = function() {
     for (i = 0; i < particles.length; i++) {
        // 粒子オブジェクト生成
        this.particles[i] = {
-    		x: 	    WINDOW_L_SIDE + Math.floor(Math.random() * WINDOW_R_SIDE),
-    		y: 	    WINDOW_TOP + Math.floor(Math.random() * WINDOW_UNDER),
-    		vx:     0,
-    		vy:     0,
-    		radius: PARTICLE_RADIUS,
-    		dens:   0.0,	// 密度 
-    		press: 	0.0,	// 圧力
-    	};
+        x:      WINDOW_L_SIDE + Math.floor(Math.random() * WINDOW_R_SIDE),
+        y:      WINDOW_TOP + Math.floor(Math.random() * WINDOW_UNDER),
+        vx:     0,
+        vy:     0,
+        radius: PARTICLE_RADIUS,
+        dens:   0.0,	// 密度 
+        press:  0.0,	// 圧力
+      };
     }
 
     // カーネル係数
@@ -95,19 +95,19 @@ function calcKernelCoefPoly6(d, type) {
     switch(type) {
       case 1: // ノーマル
         switch(DIMENSION) {
-          case 2: coefNum = 4.0/( Math.PI * Math.pow(SCOPE_H, 8) );			break;
-          case 3: coefNum = 315.0/( 64.0 * Math.PI * Math.pow(SCOPE_H, 9) ); 	break;
+          case 2: coefNum = 4.0/( Math.PI * Math.pow(SCOPE_H, 8) );           break;
+          case 3: coefNum = 315.0/( 64.0 * Math.PI * Math.pow(SCOPE_H, 9) );  break;
         }
-    		break;
+        break;
       case 2: // 勾配
         switch(DIMENSION) {
-          case 2: coefNum = -24.0/( Math.PI * Math.pow(SCOPE_H, 8) );			break;
+          case 2: coefNum = -24.0/( Math.PI * Math.pow(SCOPE_H, 8) );         break;
           case 3: coefNum = -945.0/( 32.0 * Math.PI * Math.pow(SCOPE_H, 9) ); break;
         }
         break;
       case 3: // ラプラシアン
         switch(DIMENSION) {
-          case 2: coefNum = -24.0/( Math.PI * Math.pow(SCOPE_H, 8) );			break;
+          case 2: coefNum = -24.0/( Math.PI * Math.pow(SCOPE_H, 8) );         break;
           case 3: coefNum = -945.0/( 32.0 * Math.PI * Math.pow(SCOPE_H, 9) ); break;
         }
         break;
@@ -119,8 +119,8 @@ function calcKernelCoefPoly6(d, type) {
 
 /**
  * Poly6カーネル関数値の計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
+ * @param 	{number} dist       粒子間の距離
+ * @param 	{number} coefNum    カーネル係数
  * @return 	{number} poly6関数値
 */
 function calcPoly6Func(dist, coefNum) {
@@ -134,9 +134,9 @@ function calcPoly6Func(dist, coefNum) {
 
 /**
  * Poly6カーネル関数勾配値の計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
- * @param 	{vec3}   rij			相対位置ベクトル
+ * @param 	{number} dist       粒子間の距離
+ * @param 	{number} coefNum    カーネル係数
+ * @param 	{vec3}   rij        相対位置ベクトル
  * @return 	{vec3} 	 poly6勾配値
 */
 function calcPoly6Grad(dist, coefNum, rij) {
@@ -151,8 +151,8 @@ function calcPoly6Grad(dist, coefNum, rij) {
 
 /**
  * Poly6カーネル関数ラプラシアンの計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
+ * @param 	{number} dist      粒子間の距離
+ * @param 	{number} coefNum   カーネル係数
  * @return 	{number} poly6ラプラシアン値
 */
 function calcPoly6Lap(dist, coefNum) {
@@ -170,8 +170,8 @@ function calcPoly6Lap(dist, coefNum) {
 
 /**
  * カーネル係数計算
- * @param 	{number} d 				2:2次元 3:3次元
- * @param 	{number} type			1:ノーマル 2:勾配 3:ラプラシアン
+ * @param 	{number} d         2:2次元 3:3次元
+ * @param 	{number} type      1:ノーマル 2:勾配 3:ラプラシアン
  * @return 	{number} カーネル係数値
 */
 function calcKernelCoefSpiky(d, type) {
@@ -203,8 +203,8 @@ function calcKernelCoefSpiky(d, type) {
 
 /**
  * Spikyカーネル関数値の計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
+ * @param 	{number} dist       粒子間の距離
+ * @param 	{number} coefNum    カーネル係数
  * @return 	{number} Spiky関数値
 */
 function calcSpikyFunc(dist, coefNum) {
@@ -218,9 +218,9 @@ function calcSpikyFunc(dist, coefNum) {
 
 /**
  * Spikyカーネル関数勾配値の計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
- * @param 	{vec3}   rij			相対位置ベクトル
+ * @param 	{number} dist        粒子間の距離
+ * @param 	{number} coefNum     カーネル係数
+ * @param 	{vec3}   rij         相対位置ベクトル
  * @return 	{number} Spiky勾配値
 */
 function calcSpikyGrad(dist, coefNum, rij) {
@@ -234,8 +234,8 @@ function calcSpikyGrad(dist, coefNum, rij) {
 
 /**
  * Spikyカーネル関数ラプラシアンの計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
+ * @param 	{number} dist        粒子間の距離
+ * @param 	{number} coefNum     カーネル係数
  * @return 	{number} Spikyラプラシアン値
 */
 function calcSpikyLap(dist, coefNum) {
@@ -253,8 +253,8 @@ function calcSpikyLap(dist, coefNum) {
 
 /**
  * カーネル係数計算
- * @param 	{number} d 				2:2次元 3:3次元
- * @param 	{number} type			1:ノーマル 2:勾配 3:ラプラシアン
+ * @param 	{number} d         2:2次元 3:3次元
+ * @param 	{number} type      1:ノーマル 2:勾配 3:ラプラシアン
  * @return 	{number} カーネル係数値
 */
 function calcKernelCoefVisc(d, type) {
@@ -301,9 +301,9 @@ function calcViscFunc(dist, coefNum) {
 
 /**
  * Viscカーネル関数勾配値の計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
- * @param 	{vec3}   rij			相対位置ベクトル
+ * @param 	{number} dist        粒子間の距離
+ * @param 	{number} coefNum     カーネル係数
+ * @param 	{vec3}   rij         相対位置ベクトル
  * @return 	{number} Visc勾配値
 */
 function calcViscGrad(dist, coefNum, rij) {
@@ -317,8 +317,8 @@ function calcViscGrad(dist, coefNum, rij) {
 
 /**
  * Viscカーネル関数ラプラシアンの計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
+ * @param 	{number} dist        粒子間の距離
+ * @param 	{number} coefNum     カーネル係数
  * @return 	{number} Viscラプラシアン値
 */
 function calcViscLap(dist, coefNum) {
@@ -335,8 +335,8 @@ function calcViscLap(dist, coefNum) {
 
 /**
  * カーネル係数計算
- * @param 	{number} d 				2:2次元 3:3次元
- * @param 	{number} type			1:ノーマル 2:勾配 3:ラプラシアン
+ * @param 	{number} d         2:2次元 3:3次元
+ * @param 	{number} type      1:ノーマル 2:勾配 3:ラプラシアン
  * @return 	{number} カーネル係数値
 */
 function calcKernelCoefSpline(d, type) {
@@ -371,8 +371,8 @@ function calcKernelCoefSpline(d, type) {
 
 /**
  * Splineカーネル関数値の計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
+ * @param 	{number} dist       粒子間の距離
+ * @param 	{number} coefNum    カーネル係数
  * @return 	{number} Spline関数値
 */
 function calcSplineFunc(dist, coefNum) {
@@ -388,9 +388,9 @@ function calcSplineFunc(dist, coefNum) {
 
 /**
  * Splineカーネル関数勾配値の計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
- * @param 	{vec3}   rij			相対位置ベクトル
+ * @param 	{number} dist        粒子間の距離
+ * @param 	{number} coefNum     カーネル係数
+ * @param 	{vec3}   rij         相対位置ベクトル
  * @return 	{number} Spline勾配値
 */
 function calcSplineGrad(dist, coefNum, rij) {
@@ -406,9 +406,9 @@ function calcSplineGrad(dist, coefNum, rij) {
 
 /**
  * Splineカーネル関数ラプラシアンの計算
- * @param 	{number} dist 			粒子間の距離
- * @param 	{number} coefNum		カーネル係数
- * @param 	{number} d				1:1次元 2:2次元 3:3次元
+ * @param 	{number} dist       粒子間の距離
+ * @param 	{number} coefNum    カーネル係数
+ * @param 	{number} d          1:1次元 2:2次元 3:3次元
  * @return 	{number} Splineラプラシアン値
 */
 function calcSplineLap(dist, coefNum, d) {
